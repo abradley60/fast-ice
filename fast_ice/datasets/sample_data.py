d4676e3926069a31488ea571753b8e873c1521bd
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2026-07-09 16:08:13 +0930

    Remove sample data metadata file and explicitly list metadata in sample data module

--- fast_ice/datasets/sample_data.py
+++ fast_ice/datasets/sample_data.py
@@ -1,7 +1,20 @@
-from ._sample_data_metadata import _SAMPLE_SCENES_METADATA, SAMPLE_SCENES
+import pooch
 from pathlib import Path
 from dataclasses import dataclass
 from datetime import datetime
+from fast_ice.io import SAR_UNIT
+
+# Pooch Registry
+SAMPLE_SCENES = pooch.create(
+    path=pooch.os_cache("fast_ice"),
+    base_url="https://deant-data-public-dev.s3.ap-southeast-2.amazonaws.com/fast-ice/",
+    registry={
+        "s1a_EW_20191217_hh_gamma0_db_sample.tif": "sha256:43041201e40bf1cb248f1c8ff75530164c31684a5db384f18a7a485f0162c6f5",
+        "s1a_EW_20191229_hh_gamma0_db_sample.tif": "sha256:1cd87e4fd99c2f03717c52659796757485666a625621b00716b582d2c2c70c8b",
+        "s1a_IW_20191219_hh-gamma0_db_sample.tif": "sha256:12b8b0cf5c236dda8db5e9e84dc7dfa8693b84770895aa6785dd439ac1e2e0a1",
+    },
+    env="FASTICE_DATA_DIR",
+)
 
 
 # Sample scene metadata class
@@ -10,17 +23,9 @@ class SampleScene:
     path: Path
     timestamp: datetime
     band: str
-    band_unit: str
+    band_unit: SAR_UNIT
 
-    def __str__(self) -> str:
-        return (
-            f"SampleScene\n"
-            f"  path:      {self.path}\n"
-            f"  timestamp: {self.timestamp.isoformat()}\n"
-            f"  band:      {self.band}\n"
-            f"  unit:      {self.band_unit}"
-        )
-
+    # Add to allow nice rendering in notebook
     def _repr_html_(self) -> str:
         return (
             f"<b>SampleScene</b><table>"
@@ -33,30 +38,51 @@ class SampleScene:
 
 
 def fetch_ew_20191217():
-    ew_20191217_metadata = _SAMPLE_SCENES_METADATA["ew_20191217"]
+    """Download s1a_EW_20191217_hh_gamma0_db_sample.tif stored on AWS
+
+    Returns
+    -------
+    SampleScene
+        A dataclass with the local path to the downloaded file, along with
+        the timestamp, band, and band unit for the file.
+    """
     return SampleScene(
-        path=Path(SAMPLE_SCENES.fetch(ew_20191217_metadata["filename"])),
-        timestamp=ew_20191217_metadata["timestamp"],
-        band=ew_20191217_metadata["band"],
-        band_unit=ew_20191217_metadata["band_unit"],
+        path=Path(SAMPLE_SCENES.fetch("s1a_EW_20191217_hh_gamma0_db_sample.tif")),
+        timestamp=datetime(2019, 12, 17, 11, 25, 53),
+        band="hh_gamma0",
+        band_unit="db",
     )
 
 
 def fetch_ew_20191229():
-    ew_20191217_metadata = _SAMPLE_SCENES_METADATA["ew_20191229"]
+    """Download s1a_EW_20191229_hh_gamma0_db_sample.tif stored on AWS
+
+    Returns
+    -------
+    SampleScene
+        A dataclass with the local path to the downloaded file, along with
+        the timestamp, band, and band unit for the file.
+    """
     return SampleScene(
-        path=Path(SAMPLE_SCENES.fetch(ew_20191217_metadata["filename"])),
-        timestamp=ew_20191217_metadata["timestamp"],
-        band=ew_20191217_metadata["band"],
-        band_unit=ew_20191217_metadata["band_unit"],
+        path=Path(SAMPLE_SCENES.fetch("s1a_EW_20191229_hh_gamma0_db_sample.tif")),
+        timestamp=datetime(2019, 12, 29, 11, 25, 53),
+        band="hh_gamma0",
+        band_unit="db",
     )
 
 
 def fetch_iw_20191219():
-    ew_20191217_metadata = _SAMPLE_SCENES_METADATA["iw_20191219"]
+    """Download s1a_IW_20191219_hh-gamma0_db_sample.tif stored on AWS
+
+    Returns
+    -------
+    SampleScene
+        A dataclass with the local path to the downloaded file, along with
+        the timestamp, band, and band unit for the file.
+    """
     return SampleScene(
-        path=Path(SAMPLE_SCENES.fetch(ew_20191217_metadata["filename"])),
-        timestamp=ew_20191217_metadata["timestamp"],
-        band=ew_20191217_metadata["band"],
-        band_unit=ew_20191217_metadata["band_unit"],
+        path=Path(SAMPLE_SCENES.fetch("s1a_IW_20191219_hh-gamma0_db_sample.tif")),
+        timestamp=datetime(2019, 12, 19, 12, 47, 44),
+        band="hh_gamma0",
+        band_unit="db",
     )
